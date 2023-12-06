538131e89009ddbe20a7d90e053991ce1d637b43
chandraitfc <107907323+chandraitfc@users.noreply.github.com>
2023-12-06 16:50:05 +0530

    Update config.py

--- ela/elautil/config.py
+++ ela/elautil/config.py
@@ -5,4 +5,13 @@ ELA_LOG_DIR = os.environ['ELA_LOG_DIR']
 ELA_LOG_FILE = os.environ['ELA_LOG_FILE']
 ELA_TRIGGER_DIR = os.environ['ELA_TRIGGER_DIR']
 ELA_TRIGGER_FILE = os.environ['ELA_TRIGGER_FILE']
+ELA_ASR_MODEL = os.environ['ELA_ASR_MODEL']
+ELA_ASR_INFERENCE_DEVICE = os.environ['ELA_ASR_INFERENCE_DEVICE'] 
+ELA_NLP_MODEL = os.environ['ELA_NLP_MODEL']
 PKG_UPLOAD_BASE_DIR = os.environ['PKG_UPLOAD_BASE_DIR']
+
+
+#fileops
+PKG_CSV_REL_DIR = 'data'
+PKG_AUDIO_REL_DIR = 'data/audio'
+PKG_META_LOG_DIR = 'log'
